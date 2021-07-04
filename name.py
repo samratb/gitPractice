b763673dfeed74b51debae9e69e318299a2b87e3
Samrat Bhattacharjee <mails.samrat@gmail.com>
2021-07-04 10:02:15 -0400

    Added file name.py

---
+++ name.py
@@ -0,0 +1,15 @@
+my_name = "Samrat"
+my_age = 30
+my_height=5.5
+my_weight = 64
+my_eyes = 'brown'
+my_hair= 'black'
+
+print(f"Let's talk about {my_name}.")
+#print(f"Let's talk about {my_name}.")
+print(f"My age is {my_age} years old.")
+print(f"my height is {my_height} feet.")
+
+
+
+
